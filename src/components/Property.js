9a7074b667eff934e1b34dfbaba863fd4ddcafdf
Saikat Samanta <iamsaikatsamanta@gmail.com>
2018-03-20 23:37:29 +0530

    Styleing Improvement

--- src/components/Property.js
+++ src/components/Property.js
@@ -12,19 +12,18 @@ const style={
 const Property = (props)=>{
     const r="/"+props.id;
     return (
-        <div className="media" style={style.main}>
-            <div className="img-thumbnail"><img className="mr-3" src={props.img} alt="Generic placeholder image"/></div>
+            <div className="media" style={style.main}>
+                <div className="img-thumbnail"><img className="mr-3" src={props.img} alt="Generic placeholder image"/></div>
                 <div className="media-body" style={style.cont}>
                     <h4 className="mt-0">{props.title}</h4>
                     <h5>{props.add}</h5>
                     <Link className="btn btn-primary" to={r}>MoreInfo</Link>
                 </div>
-            <Route path={r} render={()=>
-                <h1></h1>
-            }
-            />
-        </div>
-
+                <Route path={r} render={()=>
+                    <h1></h1>
+                }
+                />
+            </div>
     );
 };
 
